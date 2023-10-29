699db1ab4f1ea4f6f3ef7c5965bb33b0d9978570
DianaCarcea <100145387+DianaCarcea@users.noreply.github.com>
2023-10-29 21:03:19 +0200

    Laboratorul 3

---
+++ Lab3/Tema3.py
@@ -0,0 +1,223 @@
+import sys
+
+# Ex1
+def intersect(a, b):
+    """Intersectia dintre a si b"""
+    return set(a) & set(b)
+
+
+def union(a, b):
+    """Reuniunea dintre a si b"""
+    return set(a) | set(b)
+
+
+def difference(a, b):
+    """Diferenta dintre a si b"""
+    return set(a)-set(b)
+
+
+# Ex2
+def count_chars(string):
+    return {ch: string.count(ch) for ch in string}
+
+
+#Ex3
+def compare_dictionaries(d1, d2):
+    if not isinstance(d1, dict) or not isinstance(d2, dict):
+        print("Instantele nu sunt dictionare!", file=sys.stderr)
+        return False
+
+    if len(d1) != len(d2):
+        return False
+
+    for key1, val1 in d1.items():
+        if key1 not in d2:
+            return False
+
+        val2 = d2[key1]
+        if val1 != val2:
+            return False
+
+        if isinstance(val1, dict):
+            if not compare_dictionaries(val1, val2):
+                return False
+
+    return True
+
+
+#Ex4
+def build_xml_element(tag, content, **key_value):
+    all_key_val = ""
+    for key, value in key_value.items():
+        all_key_val = all_key_val + f'{key}="{value}" '
+    if all_key_val:
+        return f"<{tag} {all_key_val.rstrip()}>{content}</{tag}>"
+    else:
+        return f"<{tag}>{content}</{tag}>"
+
+
+#Ex5
+def validate_dict(rules, d):
+    keys_rules = [rule[0] for rule in rules]
+    for key_d, val in d.items():
+        if key_d not in keys_rules:
+            return False
+
+    for rule in rules:
+        key_rule, prefix, middle, suffix = rule
+
+        if key_rule not in d:  #cheia nu se gaseste in dictionar => ok
+            continue
+
+        val = d[key_rule]
+
+        # Middle gol -> verific prefix si suffix
+        if middle == "":
+            if val.startswith(prefix) and val.endswith(suffix):
+                continue
+            else:
+                return False
+
+        # Valoarea incepe cu prefix si se termina cu suffix
+        if not val.startswith(prefix) or not val.endswith(suffix):
+            return False
+
+        # Prefix gol, verific daca val nu incepe cu middle
+        if not prefix and val.startswith(middle):
+            return False
+
+        # Suffix gol, verific dacă val nu se termina cu middle
+        if not suffix and val.endswith(middle):
+            return False
+
+        # Eliminăm prefix si sufix și verif dacă middle este in val
+        middle_val = val[len(prefix):len(val)-len(suffix)]
+        if middle not in middle_val:
+            return False
+
+    return True
+
+
+#Ex6
+def unique_dublicate(my_list):
+    if not isinstance(my_list, list):
+        raise TypeError("Tipul datei de intrare trebuie să fie lista!")
+
+    a = {(elem, type(elem)) for elem in my_list}
+    count = 0
+
+    for elem, elem_type in a:
+        aparitii = sum(1 for item in my_list if item == elem and type(item) == elem_type)
+        if aparitii > 1:
+            count += 1
+
+    return len(a), count
+
+
+#Ex7
+def op_for_sets(*sets):
+    d = {}
+    sets_list = list(sets)
+    for i in range(len(sets_list)):
+        for j in range(i + 1, len(sets_list)):
+            set1 = sets_list[i]
+            set2 = sets_list[j]
+
+            d[f"{set1} | {set2}"] = set1 | set2 if set1 | set2 else "empty set"
+            d[f"{set1} & {set2}"] = set1 & set2 if set1 & set2 else "empty set"
+            d[f"{set1} - {set2}"] = set1 - set2 if set1 - set2 else "empty set"
+            d[f"{set2} - {set1}"] = set2 - set1 if set2 - set1 else "empty set"
+
+    return d
+
+
+#Ex8
+def loop(mapping):
+
+    if "start" not in mapping.keys():
+        raise ValueError("Nu exista cheia 'start'!")
+
+    result = []
+    key = "start"
+    while key in mapping and mapping[key] not in result:
+        result.append(mapping[key])
+        key = mapping[key]
+
+    return result
+
+
+#Ex9
+def my_function(*args, **keyword_args):
+    count = 0
+    for val in args:
+        if val in keyword_args.values():
+            count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+
+    #Ex1
+    print("\nEx1:")
+    a = ["ana", "are", 2, "mere"]
+    b = ["ion", "are", 3, "pere"]
+    print(f"Intersectia dintre a si b: {intersect(a,b)}")
+    print(f"Reuniunea dintre a si b: {union(a, b)}")
+    print(f"Diferenta dintre a si b: {difference(a, b)}")
+    print(f"Diferenta dintre b si a: {difference(b, a)}")
+
+
+    #Ex2
+    print("\nEx2:")
+    print(count_chars("Ana has apples."))
+
+    #Ex3
+    print("\nEx3:")
+    lista1 = [1, 2, 3]
+    lista2 = [1, 2, 3]
+    note1 = {"java": 8, "python": 10}
+    note2 = {"java": 8, "python": 10}
+
+    d1 = {"note": note1, "list": lista1, "nu": 0, "da": 1}
+    d2 = {"note": note2, "list": lista2, "da": 1, "nu": 0}
+    print(compare_dictionaries(d1, d2))
+
+    print("----------------")
+
+    string1 = "Ana are mere"
+    string2 = "Ion are pere"
+    d3 = {"note": note1, "str": string1, "list": lista1, "nu": 0, "da": 1}
+    d4 = {"note": note2, "list": lista2, "str": string2, "da": 1, "nu": 0}
+    print(compare_dictionaries(d3, d4))
+
+    #Ex4
+    print("\nEx4:")
+    print(build_xml_element("a", "Hello there", href="http://python.org/", _class="my-link", id="someid"))
+    print(build_xml_element("div", "Content goes here"))
+    print(build_xml_element("input", "", type="text", placeholder="Enter something"))
+
+    #Ex5
+    print("\nEx5:")
+    s = ("key1", "", "inside", ""), ("key2", "start", "middle", "winter")
+    d = {"key1": "come inside, it's too cold out", "key3": "this is not valid"}
+    print(validate_dict(s, d))
+
+    #Ex6
+    print("\nEx6:")
+    my_list = [True, False, True, 1]
+    print(unique_dublicate(my_list))
+
+    #Ex7
+    print("\nEx7:")
+    result = op_for_sets({1, 2}, {2, 3})
+    for key, value in result.items():
+        print(f"{key}: {value}")
+
+    #Ex8
+    print("\nEx8:")
+    print(loop({'start': 'a', 'b': 'a', 'a': '6', '6': 'z', 'x': '2', 'z': '2', '2': '2', 'y': 'start'}))
+
+    #Ex9
+    print("\nEx9:")
+    print(my_function(1, 2, 3, 4, x=1, y=2, z=3, w=5))
